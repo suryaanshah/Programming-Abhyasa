5c48bbc99bea63c341fc6686608aa26459a3cd6d
suryaanshchawla <47082682+suryaanshchawla@users.noreply.github.com>
2020-08-02 16:00:55 +0530

    Added comments
    I will add comments explaining the code in every file from now on. Also I will try to add comments in my previous solution files also.

--- Python.py4e/String Split and Join.py
+++ Python.py4e/String Split and Join.py
@@ -1,9 +1,9 @@
 def split_and_join(line):
-    line=line.split(" ")
-    line="-".join(line)
-    return(line)
+    line=line.split(" ") # Splitting the string to a list where each word is a list-item(for example- "Hello World" to ['Hello', 'World'])
+    line="-".join(line) # Joining the list items to a string in which each word is seperated by a "-" rather than a space.(for example- ['Hello', 'World'] to "Hello-World")
+    return(line) # Returning the output. We won't print it because if we do so, we will get two outputs those are: 1. (None) and 2. (the neccesary output)
 
 if __name__ == '__main__':
     line = input()
-    result = split_and_join(line)
+    result = split_and_join(line) # The code for checking the results.
     print(result)
