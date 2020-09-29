64a809181f1224834a328b93a5bcbbaade533c84
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2020-09-29 13:34:13 +0530

    Added more comments

--- Python.py4e/Find the percentage/Finding-the-percentage.py
+++ Python.py4e/Find the percentage/Finding-the-percentage.py
@@ -1,3 +1,4 @@
+#Given Sub-Code
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
@@ -6,6 +7,7 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
-    
+ 
+#Solution
 ans=sum(student_marks[query_name])/len(student_marks[query_name]) #Defining a variable, ans.
 print("%.2f" % ans)#Printing ans with 2 decimal places
