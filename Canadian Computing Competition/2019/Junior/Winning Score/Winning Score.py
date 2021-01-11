d713f0667d03eb645a1ec720e025b9dd0f66edf9
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2021-01-11 20:42:03 +0530

    Create Winning Score.py

---
+++ Canadian Computing Competition/2019/Junior/Winning Score/Winning Score.py
@@ -0,0 +1,19 @@
+# Getting the inputs from the user
+A3pts=int(input("Enter the no. of A3 pointers"))
+A2pts=int(input("Enter the no. of A2 pointers"))
+A1pts=int(input("Enter the no. of A1 pointers"))
+B3pts=int(input("Enter the no. of B3 pointers"))
+B2pts=int(input("Enter the no. of B2 pointers"))
+B1pts=int(input("Enter the no. of B1 pointers"))
+
+# Calculating scores
+Ascore=(A3pts*3)+(A2pts*2)+(A1pts)
+Bscore=(B3pts*3)+(B2pts*2)+(B1pts)
+
+# Giving the output
+if Ascore>Bscore:
+    print("A")
+elif Bscore>Ascore:
+    print("B")
+else:
+    print("T")
