bba59a238c7accf5651adb202351ed6b343f7905
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2021-01-14 09:09:56 +0530

    Moved Hackerrank Practices to 1 single folder

--- Python.py4e/Loops.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-
-i=0
-while i<n:
-    print(i**2)
-    i+=1
--- Python.py4e/Python-Division.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-
-# output
-print(a//b)
-print(float(a/b))
