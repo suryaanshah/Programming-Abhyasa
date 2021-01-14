b751da638c788f48929b29a1db79bdb4a9dcbabf
Suryaansh Chawla <47082682+suryaanshchawla@users.noreply.github.com>
2021-01-14 09:08:22 +0530

    Moved Hackerrank Practices to 1 folder

--- 30 days of code/Day 6/Day 6 Solution.py
+++
@@ -1,4 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-for i in range(int(input())): 
-    s=input(); #Takes the input from the user
-    print(*["".join(s[::2]),"".join(s[1::2])]) #Prints(displays) the even indexes
