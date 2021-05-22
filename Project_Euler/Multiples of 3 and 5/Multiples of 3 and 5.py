fb161eba926ab8dd543418ba6e539c4512e157b3
Suryaansh Chawla <47082682+suryaanshah@users.noreply.github.com>
2021-05-22 17:58:38 +0530

    Deleted for Rearranging

--- Project_Euler/Multiples of 3 and 5/Multiples of 3 and 5.py
+++
@@ -1,8 +0,0 @@
-# Looping and finding the multiples of 3 or 5
-MultiplesOf3n5=[]
-for i in range(1001): # 1001 so it repeats 1000 times
-    if i % 3 == 0 or i % 5 == 0:
-        MultiplesOf3n5.append(i)
-    else:
-        None
- print(sum(MultiplesOf3n5))
